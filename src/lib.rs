166c85fefdb7ac580eb984555dbad76b7ffbef9a
Andrey <amigin@gmail.com>
2021-11-19 11:14:32 +0200

    First Commit

---
+++ src/lib.rs
@@ -0,0 +1,3 @@
+mod my_telemetry;
+
+pub use my_telemetry::MyTelemetry;
---
+++ src/my_telemetry.rs
@@ -0,0 +1,18 @@
+use std::time::Duration;
+
+pub trait MyTelemetry {
+    fn track_url_duration(
+        method: hyper::Method,
+        uri: hyper::Uri,
+        http_code: u16,
+        duration: Duration,
+    );
+
+    fn track_dependency_duration(
+        dependency_name: String,
+        method: hyper::Method,
+        uri: hyper::Uri,
+        success: bool,
+        duration: Duration,
+    );
+}
