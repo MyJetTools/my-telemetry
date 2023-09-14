1fc0b5a4154ae5bf3a62f4d9d0b2d3e68c81860b
amigin <amigin@gmail.com>
2023-09-14 12:52:50 +0300

    Exposed MyTelemetryEventTag

--- src/lib.rs
+++ src/lib.rs
@@ -5,7 +5,7 @@ pub use event_duration_tracker::*;
 mod ctx;
 mod telemetry_collector;
 pub use ctx::*;
-pub use my_telemetry_event::TelemetryEvent;
+pub use my_telemetry_event::*;
 pub use telemetry_collector::TelemetryCollector;
 mod telemetry_interface;
 pub use telemetry_interface::*;
