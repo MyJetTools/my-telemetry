c2f47ab00cffae99c2f8214b2f752d39cc875719
amigin <amigin@gmail.com>
2023-09-14 14:40:35 +0300

    Added TelemetryEventTagsBuilder

--- src/my_telemetry_event.rs
+++ src/my_telemetry_event.rs
@@ -1,3 +1,5 @@
+use rust_extensions::StrOrString;
+
 pub struct TelemetryEvent {
     pub process_id: i64,
     pub started: i64,
@@ -12,4 +14,40 @@ pub struct TelemetryEvent {
 pub struct TelemetryEventTag {
     pub key: String,
     pub value: String,
+}
+
+pub struct TelemetryEventTagsBuilder {
+    pub events: Option<Vec<TelemetryEventTag>>,
+}
+
+impl TelemetryEventTagsBuilder {
+    pub fn new() -> Self {
+        Self {
+            events: Some(Vec::new()),
+        }
+    }
+
+    pub fn add(
+        &mut self,
+        key: impl Into<StrOrString<'static>>,
+        value: impl Into<StrOrString<'static>>,
+    ) {
+        if self.events.is_none() {
+            self.events = Some(Vec::new());
+        }
+        self.events.as_mut().unwrap().push(TelemetryEventTag {
+            key: key.into().into(),
+            value: value.into().into(),
+        })
+    }
+
+    pub fn build(self) -> Option<Vec<TelemetryEventTag>> {
+        self.events
+    }
+}
+
+impl Into<Option<Vec<TelemetryEventTag>>> for TelemetryEventTagsBuilder {
+    fn into(self) -> Option<Vec<TelemetryEventTag>> {
+        self.build()
+    }
 }
