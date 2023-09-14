db60adbf8f76c6df878e65b2517f7f9fa67875d0
RsSvc <wonderhays@gmail.com>
2023-09-14 21:05:16 +0300

    exports writer as pub crate

--- my-telemetry/src/lib.rs
+++ my-telemetry/src/lib.rs
@@ -3,6 +3,4 @@ extern crate my_telemetry_core;
 pub use my_telemetry_core::*;
 
 #[cfg(feature = "my-telemetry-writer")]
-extern crate my_telemetry_writer;
-#[cfg(feature = "my-telemetry-writer")]
-pub use my_telemetry_writer::*;
+pub extern crate my_telemetry_writer;
