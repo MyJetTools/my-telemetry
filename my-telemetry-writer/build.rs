e560d0178a6fa9ff8f7ccf70202643e37a4cade2
amigin <amigin@gmail.com>
2024-05-29 15:05:35 +0300

    Fixing

--- my-telemetry-writer/build.rs
+++ my-telemetry-writer/build.rs
@@ -1,4 +1,7 @@
 fn main() {
+    let mut config = prost_build::Config::new();
+    config.protoc_arg("--experimental_allow_proto3_optional");
+
     let url = "https://raw.githubusercontent.com/MyJetTools/my-telemetry-server/main/proto/";
     ci_utils::sync_and_build_proto_file(url, "TelemetryWriter.proto");
 }
