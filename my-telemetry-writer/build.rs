fae4bb636104874c9327cff01e2143d13dcb58bc
amigin <amigin@gmail.com>
2025-02-19 15:39:32 +0200

    Made it compilable

--- my-telemetry-writer/build.rs
+++ my-telemetry-writer/build.rs
@@ -1,4 +1,7 @@
 fn main() {
-    let url = "https://raw.githubusercontent.com/MyJetTools/my-telemetry-server/main/proto/";
-    ci_utils::sync_and_build_proto_file(url, "TelemetryWriter.proto");
+    //let url =
+    // "https://raw.githubusercontent.com/MyJetTools/my-telemetry-server/refs/heads/main/proto/";
+    // ci_utils::sync_and_build_proto_file(url, "TelemetryWriter.proto");
+
+    tonic_build::compile_protos("proto/TelemetryWriter.proto").unwrap();
 }
