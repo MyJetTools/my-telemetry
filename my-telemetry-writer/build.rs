c54247e495fe1d45006a3c3eaefb47ae8c55a863
Andrey <amigin@gmail.com>
2025-06-22 10:16:24 +0300

    Fixes

--- my-telemetry-writer/build.rs
+++ my-telemetry-writer/build.rs
@@ -3,5 +3,11 @@ fn main() {
     // "https://raw.githubusercontent.com/MyJetTools/my-telemetry-server/refs/heads/main/proto/";
     // ci_utils::sync_and_build_proto_file(url, "TelemetryWriter.proto");
 
-    tonic_build::compile_protos("proto/TelemetryWriter.proto").unwrap();
+    let includes: Vec<String> = Vec::new();
+    tonic_build::configure()
+        .protoc_arg("--experimental_allow_proto3_optional")
+        .compile_protos(&["proto/TelemetryWriter.proto"], &includes)
+        .unwrap();
+
+    //    tonic_build::compile_protos("proto/TelemetryWriter.proto").unwrap();
 }
