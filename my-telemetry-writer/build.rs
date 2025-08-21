6633c5dceab74be45c83b3ab70fe8021de0e97ac
amigin <amigin@gmail.com>
2025-08-21 10:47:43 +0300

    Last version of tonic

--- my-telemetry-writer/build.rs
+++ my-telemetry-writer/build.rs
@@ -1,13 +1,3 @@
 fn main() {
-    //let url =
-    // "https://raw.githubusercontent.com/MyJetTools/my-telemetry-server/refs/heads/main/proto/";
-    // ci_utils::sync_and_build_proto_file(url, "TelemetryWriter.proto");
-
-    let includes: Vec<String> = Vec::new();
-    tonic_build::configure()
-        .protoc_arg("--experimental_allow_proto3_optional")
-        .compile_protos(&["proto/TelemetryWriter.proto"], &includes)
-        .unwrap();
-
-    //    tonic_build::compile_protos("proto/TelemetryWriter.proto").unwrap();
+    tonic_prost_build::compile_protos("proto/TelemetryWriter.proto").unwrap();
 }
