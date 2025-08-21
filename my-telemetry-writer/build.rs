a6207c29911a7f04d038f6d718c3a9442b25672c
amigin <amigin@gmail.com>
2025-08-21 12:05:24 +0300

    Fixes

--- my-telemetry-writer/build.rs
+++ my-telemetry-writer/build.rs
@@ -1,3 +1,6 @@
 fn main() {
-    tonic_prost_build::compile_protos("proto/TelemetryWriter.proto").unwrap();
+    tonic_prost_build::configure()
+        .protoc_arg("--experimental_allow_proto3_optional")
+        .compile_protos(&["proto/TelemetryWriter.proto"], &["proto"])
+        .unwrap();
 }
